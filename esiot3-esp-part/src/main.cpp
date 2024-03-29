4e1ba30b7a7d92e5fab967a98646525406164a52
penazziriccardo <penazziriccardo17@gmail.com>
2024-03-29 11:52:47 +0100

    esp connected to wifi correctly

--- esiot3-esp-part/src/main.cpp
+++ esiot3-esp-part/src/main.cpp
@@ -1,16 +1,24 @@
 #include <Arduino.h>
 #include <Wire.h>
+#include <WiFi.h>
 
 #define ECHO_PIN 33
 #define TRIG_PIN 32
 
 long duration, distance;
 
+const char *ssid = "ASUS";
+const char *psw = "lucia1999";
+
+void setup_wifi();
+
 void setup()
 {
 	Serial.begin(9600);
 	pinMode(TRIG_PIN, OUTPUT);
 	pinMode(ECHO_PIN, INPUT);
+
+	setup_wifi();
 }
 
 void loop()
@@ -25,4 +33,26 @@ void loop()
 	distance = duration / 58.2;
 	Serial.println("Distance: " + String(distance));
 	delay(1000);
+}
+
+void setup_wifi()
+{
+
+	delay(10);
+
+	Serial.println(String("Connecting to ") + ssid);
+
+	WiFi.mode(WIFI_STA);
+	WiFi.begin(ssid, psw);
+
+	while (WiFi.status() != WL_CONNECTED)
+	{
+		delay(500);
+		Serial.print(".");
+	}
+
+	Serial.println("");
+	Serial.println("WiFi connected");
+	Serial.println("IP address: ");
+	Serial.println(WiFi.localIP());
 }
