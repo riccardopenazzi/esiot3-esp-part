f042ea01d03058ca059f9cd66948897f5c4bbbfd
penazziriccardo <penazziriccardo17@gmail.com>
2024-03-29 12:09:26 +0100

    status led working corretcly showing if network is ok or not

--- esiot3-esp-part/src/main.cpp
+++ esiot3-esp-part/src/main.cpp
@@ -4,24 +4,36 @@
 
 #define ECHO_PIN 33
 #define TRIG_PIN 32
+#define GREEN_LED 18
+#define RED_LED 19
 
 long duration, distance;
 
 const char *ssid = "ASUS";
 const char *psw = "lucia1999";
 
+void sonar_monitor();
 void setup_wifi();
+void check_network();
 
 void setup()
 {
 	Serial.begin(9600);
 	pinMode(TRIG_PIN, OUTPUT);
 	pinMode(ECHO_PIN, INPUT);
-
+	pinMode(GREEN_LED, OUTPUT);
+	pinMode(RED_LED, OUTPUT);
+	digitalWrite(RED_LED, HIGH);
 	setup_wifi();
 }
 
 void loop()
+{
+	check_network();
+	sonar_monitor();
+}
+
+void sonar_monitor()
 {
 	digitalWrite(TRIG_PIN, LOW);
 	delayMicroseconds(2);
@@ -37,22 +49,27 @@ void loop()
 
 void setup_wifi()
 {
-
 	delay(10);
-
 	Serial.println(String("Connecting to ") + ssid);
-
 	WiFi.mode(WIFI_STA);
 	WiFi.begin(ssid, psw);
-
 	while (WiFi.status() != WL_CONNECTED)
 	{
-		delay(500);
-		Serial.print(".");
 	}
-
-	Serial.println("");
 	Serial.println("WiFi connected");
-	Serial.println("IP address: ");
-	Serial.println(WiFi.localIP());
+	digitalWrite(GREEN_LED, HIGH);
+}
+
+void check_network()
+{
+	if (WiFi.status() == WL_CONNECTED)
+	{
+		digitalWrite(RED_LED, LOW);
+		digitalWrite(GREEN_LED, HIGH);
+	}
+	else
+	{
+		digitalWrite(GREEN_LED, LOW);
+		digitalWrite(RED_LED, HIGH);
+	}
 }
